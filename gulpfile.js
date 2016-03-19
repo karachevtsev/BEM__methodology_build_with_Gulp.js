fdca1e00ba28aed06be22c5759cf303ddc813424
karachevtsev <karachevtsev@yahoo.com>
2016-03-19 20:53:38 +0200

    Fixed gulpfile. Build project done

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,65 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
     browserSync = require('browser-sync').create(),
-    reload = browserSync.reload;
+    reload = browserSync.reload,
+    path = require('path'),
+    url = require('gulp-css-url-adjuster'),
+    autoprefixer = require('autoprefixer-core'),
+    postcss = require('gulp-postcss');
 
 var params = {
     out: 'public/',
     htmlSrc: 'index.pink.html',
-    levels: ['pink.blocks','common.blocks']
-};
+    levels: ['common.blocks', 'pink.blocks']
+},
+    getFileNames = require('html2bl').getFileNames(params);
 
 gulp.task('default', ['server', 'build']);
 
-gulp.task('build', ['html', 'css']);
+gulp.task('build', ['html', 'css', 'images']);
 
 gulp.task('server', function() {
     browserSync.init({
         server: params.out
     });
 
-    gulp.watch('*.html', ['html', 'css']);
+    gulp.watch('*.html', ['html']);
 
+    gulp.watch(params.levels.map(function(level) {
+        var cssGlob = level + '/**/*.css';
+        return cssGlob;
+    }), ['css']);
 });
 
 gulp.task('html', function() {
     gulp.src(params.htmlSrc)
-    .pipe(rename('index.html'))
-    .pipe(gulp.dest(params.out))
-    .pipe(reload({ stream: true }));
+    .pipe( rename('index.html') )
+    .pipe( gulp.dest(params.out) )
+    .pipe( reload( { stream: true } ) );
 });
 
 gulp.task('css', function() {
-    gulp.src(['common.blocks/**/*.css', 'pink.blocks/**/*.css'])
-    .pipe(concat('styles.css'))
-    .pipe(gulp.dest(params.out))
-    .pipe(reload({ stream: true }));
+    getFileNames.then(function(files) {
+        return gulp.src(files.css)
+            .pipe( concat('styles.css') )
+            .pipe(url({
+                prepend: 'images/'
+            }))
+            .pipe( postcss([ autoprefixer() ]) )
+            .pipe( gulp.dest(params.out) )
+            .pipe( reload( { stream: true } ) );
+    })
+    .done();
+});
+
+gulp.task('images', function() {
+    getFileNames.then(function(src) {
+        gulp.src(src.dirs.map(function(dirName) {
+            var imgGlob = path.resolve(dirName) + '/*.{jpg,png,svg}';
+            console.log(imgGlob);
+            return imgGlob;
+        }))
+        .pipe( gulp.dest( path.join(params.out + '/images/') ) );
+    })
+    .done();
 });
